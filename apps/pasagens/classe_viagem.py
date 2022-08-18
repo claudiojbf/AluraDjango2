7dfd5a5c6d900433b32cc1dcb19ba4066241d83a
Claudio Jose Bezerra Filho <65622389+claudiojbf@users.noreply.github.com>
2022-08-18 19:37:22 -0300

    Acrecimo de mais alguns campos

---
+++ apps/pasagens/classe_viagem.py
@@ -0,0 +1,5 @@
+tipo_de_classe = {
+    (1, 'Economica'),
+    (2, 'Executiva'),
+    (3, 'Primeira Classe')
+}
--- apps/pasagens/forms.py
+++ apps/pasagens/forms.py
@@ -1,8 +1,19 @@
+import email
 from django import forms
 from tempus_dominus.widgets import DatePicker
-
+from datetime import datetime
+from .classe_viagem import tipo_de_classe
 class PassagemForms(forms.Form):
     origem = forms.CharField(label='Origem', max_length=100)
     destino = forms.CharField(label='Destino', max_length=100)
     data_ida = forms.DateField(label='Ida', widget= DatePicker())
     data_volta = forms.DateField(label='Volta', widget= DatePicker())
+    data_pesquisa = forms.DateField(label='Data da Pesquisa', disabled=True, initial=datetime.today)
+    classe_viagens = forms.ChoiceField(label='Classe do Vôo', choices=tipo_de_classe)
+    informacoes = forms.CharField(
+        label='Informações extras',
+        max_length=200,
+        widget= forms.Textarea(),
+        required=False 
+    )
+    email = forms.EmailField(label='Email', max_length=150)
